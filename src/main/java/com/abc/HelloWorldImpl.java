748f945953827403e915fb129d0c983ddf47a5ef
jackson18 <qijiabin18@126.com>
2016-03-09 15:50:46 +0800

    Thrift简单示例

---
+++ src/main/java/com/abc/HelloWorldImpl.java
@@ -0,0 +1,15 @@
+package com.abc;
+ 
+import org.apache.thrift.TException;
+ 
+public class HelloWorldImpl implements HelloWorldService.Iface {
+ 
+    public HelloWorldImpl() {
+    }
+ 
+    @Override
+    public String sayHello(String username) throws TException {
+        return "hello, " + username;
+    }
+ 
+}
